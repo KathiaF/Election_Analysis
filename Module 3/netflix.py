3f8b78c7aced9d758d67a745a60b92b7bf2d0b40
KathF <kathia.floresrd@uanl.edu.com>
2022-01-20 13:23:04 -0600

    Module 3 Challenge

--- Module 3/netflix.py
+++ Module 3/netflix.py
@@ -2,27 +2,50 @@
 import os
 import csv
 
-#Read
 # Prompt user for video lookup
 video = input("What show or movie are you looking for? ")
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "netflix_ratings.csv")
+#csvpath = os.path.join("..", "Resources", "netflix_ratings.csv")
+csvpath = os.path.join("Resources", "netflix_ratings.csv")
 
-# Open the CSV
-with open(csvpath) as csvfile:
+# Specify the file to write to the movie data.
+#txtpath = os.path.join("..", "output", "netflix_data.txt")
+txtpath = os.path.join("output", "netflix_data.txt")
+
+# Set a variable to false to check if we found the video
+found = False
+
+# Open the NetFlix CSV
+with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader)
+
     # Loop through looking for the video
     for row in csvreader:
         if row[0] == video:
-            print(f"{row[0]} is rated {row[1]} with a rating of {row[5]}")
-            
-#WRITE
-file_outpath = os.path.join("..", "output", "Employee_Data.txt")
 
-#w:write, a:append
-with open(file_outpath, "w") as textfile:
-    employee_data = (
-        
-    )
+            # Set the variable created to confirm we have found the video
+            found = True
+
+            # Open the file using "write" mode. Specify the variable to hold the contents
+            with open(txtpath, 'w') as textfile:
+                
+                # Create a variable to hold the title, rating level, and the movie rating inside parentheses.
+                netflix_data = (
+                    f"Title: {row[0]}\n"
+                    f"Rating Level: {row[1]}\n"
+                    f"Rated: {row[5]}\n"
+                    )
+                
+                # Write the movie data to the text file.
+                textfile.write(netflix_data)
+
+                # Print the movie data to the console.
+                print(netflix_data)
+
+            # Stop at first results to avoid duplicates
+            break
+
+    # If the video is never found, alert the user
+    if found is False:
+        print("Sorry about this, we don't seem to have what you are looking for!")
